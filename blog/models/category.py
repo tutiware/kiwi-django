2f4842d057082264b42d5040cef13c996425163b
tutiware <c.tr.muslu@gmail.com>
2022-02-26 16:42:41 +0300

    all ok till article model

---
+++ blog/models/category.py
@@ -0,0 +1,21 @@
+from django.db import models
+from autoslug import AutoSlugField
+
+class categoryModel(models.Model):
+    title = models.CharField( #defines a name made of characters
+        max_length = 30, #limits char length of title
+        blank = False, #sets whether you'are letting the name to be " "
+        null = False #sets whether you'are letting the name undefined
+        )
+    slug = AutoSlugField( #defines URL of the category like site.com/category/catg_1
+        populate_from ='title', #sets title of the category as URL like category/<title>
+        unique = True #in case of conflict with existed URL, it generates a new unique one
+    )
+
+    class Meta: #meta class lets you set additional specialized options into your model
+        db_table = 'category' #sets the table name
+        verbose_name = 'category' #specifies the name of your model on panel.
+        verbose_name_plural = 'categories' #for the case plural examples take place
+
+    def __str__(self):
+        return self.title #provides model examples in server to shown as their 'title's
